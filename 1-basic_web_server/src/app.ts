9034f61cb121247ce5baaa8d2243ae6114f08c24
aniebietafia <aniebietafia87@gmail.com>
2024-06-22 21:01:55 +0100

    setting up the server

---
+++ 1-basic_web_server/src/app.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import { Request, Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { StatusCodes } from "http-status-codes";
+
+// instanciate express app
+const app: express.Application = express();
+
+// define a route
+
+// setup middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// mount routes
+
+// define index route
+app.get("/", (_req: Request, res: Response) => {
+  res.status(StatusCodes.OK).json({
+    message: "Hello World",
+  });
+});
+
+// define not found route
+app.all("*", (_req: Request, res: Response) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    message: "Resource not found",
+  });
+});
+
+// global error handler
+app.use((err: Error, _req: Request, res: Response, _next: Function) => {
+  console.error(err.stack);
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    message: "Internal server error",
+  });
+});
+
+export default app;
---
+++ 1-basic_web_server/src/server.ts
@@ -0,0 +1,37 @@
+import http from "node:http";
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app";
+
+// set port
+const PORT: string | number = process.env["PORT"] || 3001;
+
+app.set("port", PORT);
+
+// create server
+export const server: http.Server = http.createServer(app);
+
+// listen
+export const start = (): void => {
+  try {
+    server.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    );
+    server.on("error", (error: Error) => {
+      throw new Error(`Error: ${error}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
+
+// gracefully shutdown
+process.on("SIGINT", () => {
+  server.close(() => {
+    console.log("Server closed");
+    process.exit(0);
+  });
+});
